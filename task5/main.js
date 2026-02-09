11149a459beae3f52349c1a0c9628ea8d5a9238d
Adams009 <140971777+Adams009@users.noreply.github.com>
2026-02-09 07:06:22 +0100

    Update main.js

--- task5/main.js
+++ task5/main.js
@@ -1,11 +1,8 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-import express from "express";
 import { GoogleGenAI } from "@google/genai";
 
-const app = express();
-app.use(express.json());
 
 // ---------------------------------------------
 // GOOGLE GEN AI SETUP
